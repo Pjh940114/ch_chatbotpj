bb673c2a555f63a74b9376cb5fe1786b796495da
Pjh940114 <pjh940114@gmail.com>
2022-06-08 22:14:12 +0900

    commit_220608

--- make_data.py
+++ make_data.py
@@ -4,23 +4,13 @@ import re
 # 슬롯 : 종류, 도수, 맛, 향 
 beer_types = ['/type;에일/', '/type;IPA/', '/type;라거/', '/type;바이젠/', '/type;흑맥주/']
 
-# for word in beer_types: # 한글만
-#     code = ord(word[-2])
-#     if 44032 <= code <= 55203 and (code - ord("가")) % 28 > 0:
-#         print(word)
-
 beer_abv = ['/abv;3도/', '/abv;4도/', '/abv;5도/', '/abv;6도/', '/abv;7도/', '/abv;8도/',
             '/abv;3도이상/', '/abv;4도이상/', '/abv;5도이상/', '/abv;6도이상/', '/abv;7도이상/',
            '/abv;3도 이상/', '/abv;4도 이상/', '/abv;5도 이상/', '/abv;6도 이상/', '/abv;7도 이상/',
            '/abv;4도이하/', '/abv;5도이하/', '/abv;6도이하/', '/abv;7도이하/', '/abv;8도이하/',
             '/abv;4도 이하/', '/abv;5도 이하/', '/abv;6도 이하/', '/abv;7도 이하/', '/abv;8도 이하/']
-
-# for word in beer_abv:
-#     if (ord(word[-2]) - ord("가")) % 28 > 0:
-#         print(word)
-
-beer_flavor = ['/flavor;과일/', '/flavor;홉/', '/flavor;꽃/', '/flavor;상큼한/', '/flavor;커피/', '/flavor;스모키한/']
-        
+beer_flavor = ['/flavor;과일/', '/flavor;홉/', '/flavor;꽃/', '/flavor;상큼한/', '/flavor;커피/', '/flavor;스모키한/']  
+     
 beer_taste = [
         '/taste;단/', '/taste;달달한/', '/taste;달콤한/',
         '/taste;안단/', '/taste;안 단/', '/taste;달지 않은/', '/taste;달지않은/',
@@ -39,20 +29,42 @@ def slots_txt(filename):
     
     f = open(filename, 'r', encoding = 'utf-8')
     lines = f.readlines()
-        
+    
+    rnd_types = random.choice(beer_types)
+    rnd_abv = random.choice(beer_abv)
+    rnd_flavor = random.choice(beer_flavor)
+    rnd_taste = random.choice(beer_taste)
+
     for line in lines:
         # txt 파일 내 주석 제거
         if "#" in line:
             line = line.replace(line,"")
-        if "{1}" or "{2}" or "{3}" or "{4}" in line:
-            line = line.replace("{1}", random.choice(beer_types)).strip()    
-            line = line.replace("{2}", random.choice(beer_abv)).strip()
-
-            line = line.replace("{3}", random.choice(beer_flavor)).strip()
-            line = line.replace("{4}", random.choice(beer_taste)).strip()
             
-            # 정규 표현식 맛, 향 명사 표현             
+        if "{1}" or "{2}" or "{3}" or "{4}" in line:
+            
+            line = line.replace("{1}", rnd_types).strip()
+            
+            # 맥주 종류 받침   
+            # code = ord(rnd_types[-2])
+            # if 44032 <= code <= 55203 and (code - ord("가")) % 28 > 0:
+            #     pass
+
+            line = line.replace("{2}", rnd_abv).strip()
+            
+            # 맥주 도수 받침
+            # if (ord(rnd_abv[-2]) - ord("가")) % 28 > 0:
+                # pass
+            
+            line = line.replace("{3}", rnd_flavor).strip()
+            line = line.replace("{4}", rnd_taste).strip()
+            
+            # 정규 표현식
+            line = re.sub(re.compile(r'/type;흑맥주/\s*맥주+\s*'), '/type;흑맥주/', line)
+            line = re.sub(re.compile(r'/type;바이젠/\s*로'), '/type;바이젠/으로', line)
+                      
             line = re.sub(re.compile(r'/taste;과일/\s*(거|걸로|게|건데|으로|것)'), '/taste;과일/맛 나는 거', line)
+            line = re.sub(re.compile(r'/taste;과일/\s*(거|걸로|게|건데|으로|것)'), '/taste;과일/맛 나는 거', line)
+            
             line = re.sub(re.compile(r'/flavor;과일/\s*(거|걸로|게|건데|으로|것)'), '/flavor;과일/향 나는 거', line)
             line = re.sub(re.compile(r'/flavor;홉/\s*(거|걸로|게|건데|으로|것)'), '/flavor;홉/향 나는 거', line)
             line = re.sub(re.compile(r'/flavor;꽃/\s*(거|걸로|게|건데|으로|것)'), '/flavor;꽃/향 나는 거', line)
